dbade3e1ea056080e37077a9ee327f7f5b2c3c8c
Vyacheslav Bolotov <tier56@gmail.com>
2023-06-19 07:08:29 +0500

    Add style, img, pages, router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import { Header } from './components';
+import { AboutPage, CartPage, CatalogPage, ContactsPage, HomePage, Page404 } from "./pages";
 
 function App() {
+
   return (
     <div className="App">
-      App
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/404" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { HomePage } from './HomePage';
+export { CatalogPage } from './CatalogPage';
+export { AboutPage } from './AboutPage';
+export { ContactsPage } from './ContactsPage';
+export { CartPage } from './CartPage';
+export { Page404 } from './Page404';
+
