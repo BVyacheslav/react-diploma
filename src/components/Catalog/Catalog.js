1cc717703fb096c1b75ee3a4f95774a15c8562c4
Vyacheslav Bolotov <tier56@gmail.com>
2023-06-21 07:43:00 +0500

    Changed catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,17 +1,3 @@
-// export function Catalog() {
-//   return (
-//     <section className="catalog">
-//       <h2 className="text-center">Каталог</h2>
-//       <div className="preloader">
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//       </div>
-//     </section>
-//   );
-// }
-
 import { Link } from "react-router-dom";
 import { useGetItemsQuery } from "../../store";
 
@@ -19,33 +5,33 @@ export function Catalog() {
   const { data: items = [], isLoading } = useGetItemsQuery();
 
   return (
-        <section className="catalog">
-          <h2 className="text-center">Каталог</h2>
-          {isLoading ?
-            <div className="preloader">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            :
-            <div className="row">
-              {items.map(product => (
-                <div key={product.id} className="col-4">
-                  <div className="card">
-                    <img src={product.images[0]}
-                      className="card-img-top img-fluid" alt={product.title} />
-                    <div className="card-body">
-                      <p className="card-text">{product.title}</p>
-                      <p className="card-text">{product.price} руб.</p>
-                      <Link to="/products/1.html" className="btn btn-outline-primary">Заказать</Link>
-                    </div>
-                  </div>
+    <section className="catalog">
+      <h2 className="text-center">Каталог</h2>
+      {isLoading ?
+        <div className="preloader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        :
+        <div className="row">
+          {items.map(product => (
+            <div key={product.id} className="col-4">
+              <div className="card">
+                <img src={product.images[0]}
+                  className="card-img-top img-fluid" alt={product.title} />
+                <div className="card-body">
+                  <p className="card-text">{product.title}</p>
+                  <p className="card-text">{product.price} руб.</p>
+                  <Link to="/products/1.html" className="btn btn-outline-primary">Заказать</Link>
                 </div>
-              ))
-              }
+              </div>
             </div>
+          ))
           }
-        </section>
+        </div>
+      }
+    </section>
   );
 }
